b0cf11d0e05182ce36b6c5d65c7d68390b37c059
Emily Mai <emily.vy.mai@gmail.com>
2023-11-29 15:24:18 -0600

    top salesperson

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,34 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const orders = getOrders()
+        const itemClicked = clickEvent.target
+        let productsSold = 0
+        
+        if (itemClicked.dataset.type === "employee") {
+            for (const order of orders) {
+                if (parseInt(itemClicked.dataset.id) === order.employeeId) {
+                    productsSold ++
+                }
+            }
+            window.alert(`${itemClicked.dataset.name} sold ${productsSold} product(s).`)
+        }
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-type="employee"
+                     data-id="${employee.id}"
+                     data-name="${employee.name}"
+                     data-rate="${employee.hourlyRate}"
+                >${employee.name}</li>`
     }
 
     html += "</ul>"
