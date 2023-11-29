107a4cd36ad4dcf66b6d063ce2b5463684de1ae3
Emily Mai <emily.vy.mai@gmail.com>
2023-11-29 14:53:59 -0600

    show product pricing on click

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,11 +2,28 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        const clickTargetData = clickTarget.dataset
+
+        if (clickTargetData.type === "product") {
+            window.alert(`${clickTargetData.name} costs \n$${(Math.round(clickTargetData.price * 100) / 100).toFixed(2)}`)
+        }
+
+    }
+)
+
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product"
+                    data-id="${product.id}"
+                    data-name="${product.name}"
+                    data-price="${product.price}"
+                >${product.name}</li>`
     }
 
     html += "</ul>"
